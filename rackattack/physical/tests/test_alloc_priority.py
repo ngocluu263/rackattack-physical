32748da24e3e979a041b96c1ba9844832d3becaa
Eliran Levi <eliran@stratoscale.com>
2015-06-01 17:17:52 +0300

    Move fake objects to common.py

--- rackattack/physical/tests/test_alloc_priority.py
+++ rackattack/physical/tests/test_alloc_priority.py
@@ -1,55 +1,8 @@
 import unittest
+from rackattack import api
 from rackattack.common import globallock
 from rackattack.physical.alloc import priority
-from rackattack import api
-
-
-class Empty:
-    pass
-
-
-class HostStateMachine:
-    def __init__(self, name):
-        self.name = name
-
-    def hostImplementation(self):
-        return Host(self.name)
-
-
-class Host:
-    def __init__(self, name):
-        self.name = name
-
-    def fulfillsRequirement(self, requirement):
-        return True
-
-
-class FreePool:
-    def __init__(self):
-        self.pool = []
-
-    def all(self):
-        return self.pool
-
-    def takeOut(self, stateMachine):
-        self.pool.remove(stateMachine)
-
-
-class Allocation:
-    def __init__(self, freePool, nice):
-        self._allocationInfo = api.AllocationInfo(user='test', purpose='user', nice=nice).__dict__
-        self.freePool = freePool
-        self.allocatedHosts = []
-
-    def withdraw(self, ignoredMessage):
-        self.freePool.pool += self.allocatedHosts
-        self.allocatedHosts = None
-
-    def allocated(self):
-        return {str(x): x for x in self.allocatedHosts}
-
-    def allocationInfo(self):
-        return self._allocationInfo
+from rackattack.physical.tests.common import HostStateMachine, FreePool, Allocation
 
 
 class Test(unittest.TestCase):
