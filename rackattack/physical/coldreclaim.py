66f22d48b9836ebef834f23d36bcaa2b59661d0b
eliran <eliran@stratoscale.com>
2015-12-24 09:53:15 +0000

    Bugfix: Too many retries on cold reclamation

--- rackattack/physical/coldreclaim.py
+++ rackattack/physical/coldreclaim.py
@@ -7,8 +7,6 @@ from rackattack.physical.ipmi import IPMI
 
 
 class ColdReclaim:
-    _MAX_NR_RETRIES = 5
-    _RETRY_INTERVAL = 10
     _CONCURRENCY = 8
     _pool = None
 
@@ -19,20 +17,15 @@ class ColdReclaim:
         self._hardReset = hardReset
         if ColdReclaim._pool is None:
             ColdReclaim._pool = multiprocessing.pool.ThreadPool(self._CONCURRENCY)
-        callback = self._run
-        ColdReclaim._pool.apply_async(callback)
+        ColdReclaim._pool.apply_async(self._run)
 
     def _run(self):
         ipmi = IPMI(self._hostname, self._username, self._password)
-        for retry in xrange(self._MAX_NR_RETRIES):
-            try:
-                if self._hardReset == "True":
-                    ipmi.powerCycle()
-                else:
-                    ipmi.softReset()
-                return
-            except:
-                logging.exception("Unable to reclaim by cold restart '%(hostname)s'",
-                                  dict(hostname=self._hostname))
-                time.sleep(self._RETRY_INTERVAL)
-        raise Exception("Cold restart retries exceeded '%(hostname)s'" % dict(hostname=self._hostname))
+        try:
+            if self._hardReset == "True":
+                ipmi.powerCycle()
+            else:
+                ipmi.softReset()
+        except:
+            logging.exception("Unable to reclaim by cold restart '%(hostname)s'",
+                              dict(hostname=self._hostname))
