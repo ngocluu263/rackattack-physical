a5622fcbaac8062e86764c2ec6949ddc943b3d59
Eliran Levi <vandelayind@gmail.com>
2015-12-25 18:20:53 +0200

    Add all modules in repository (apart from some exceptions in blacklist) to coverage listing

--- Makefile
+++ Makefile
@@ -4,10 +4,9 @@ all: check_requirements unittest build check_convention
 clean:
 	sudo rm -fr build
 
-COVERED_FILES=rackattack/physical/alloc/priority.py,rackattack/physical/dynamicconfig.py,rackattack/physical/alloc/freepool.py,rackattack/physical/alloc/allocation.py,rackattack/physical/host.py,rackattack/physical/alloc/allocations.py,rackattack/physical/network.py,rackattack/physical/host.py
 unittest: check_requirements
 	@UPSETO_JOIN_PYTHON_NAMESPACES=Yes PYTHONPATH=. python -m coverage run -m rackattack.physical.tests.runner
-	@python -m coverage report --show-missing --fail-under=86 --include=$(COVERED_FILES)
+	@python -m coverage report --show-missing --fail-under=75 --include=rackattack/* --omit="rackattack/physical/tests/*"
 
 .PHONY: integration_test
 integration_test:
--- rackattack/physical/tests/runner.py
+++ rackattack/physical/tests/runner.py
@@ -1,6 +1,8 @@
 import os
 import logging
 import unittest
+import itertools
+import rackattack
 
 
 logLevels = {0: logging.CRITICAL + 1,
@@ -22,7 +24,24 @@ def configureLogging(verbosity):
     logger.setLevel(logLevel)
 
 
+def importAllModulesToMakeThemAppearInCoverageReport():
+    blackList = ["rackattack.physical.logconfig"]
+    dirLists = [[os.path.join(item[0], filename) for filename in item[2]] for item in os.walk("rackattack")]
+    files = list(itertools.chain(*dirLists))
+    pythonFiles = [_file for _file in files if _file.endswith(".py")]
+    testPath = os.path.join("rackattack", "physical", "tests")
+    productionFiles = [_file for _file in pythonFiles if not _file.startswith(testPath)]
+    productionFilesWithoutMain = [_file for _file in productionFiles if "main" not in
+                                  os.path.basename(_file)]
+    modules = [_file.replace(os.path.sep, ".").split(".py")[0] for _file in productionFilesWithoutMain]
+    modules = [module for module in modules if module not in blackList]
+    for module in modules:
+        if module.endswith(".__init__"):
+            module = module.rstrip(".__init__")
+        __import__(module)
+
 if __name__ == "__main__":
+    importAllModulesToMakeThemAppearInCoverageReport()
     if "VERBOSITY" not in os.environ:
         maxVerbosity = max(logLevels.keys())
         print "Note: For different verbosity levels, run with VERBOSITY=(number from 0 to " \
