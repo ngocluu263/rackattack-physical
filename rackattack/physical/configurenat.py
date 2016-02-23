72308bcfe622d8d71eab22471a0c7359f312cdd2
Eliran Levi <eliran@stratoscale.com>
2016-02-23 21:36:37 +0200

    Fix the configurenat script

--- rackattack/physical/configurenat.py
+++ rackattack/physical/configurenat.py
@@ -1,7 +1,9 @@
 import sys
+import yaml
 import logging
 import subprocess
-from rackattack.physical.network import sshPortFromHostIndex, ipAddressFromHostIndex, LAST_INDEX
+from rackattack.physical import config
+from rackattack.physical import network
 
 
 def runCmd(cmd, canFail):
@@ -13,34 +15,37 @@ def runCmd(cmd, canFail):
             logging.info(result)
 
 
-def deleteRules(deviceName):
-    for index in xrange(1, LAST_INDEX):
-        cmd = ["iptables", '-D', 'PREROUTING', '-t', 'nat', '-i', deviceName,
-               "-p", "tcp", "--dport", str(sshPortFromHostIndex(index)), "-j", "DNAT",
-               "--to-destination", "%s:22" % ipAddressFromHostIndex(index)]
+def deleteRules(interface):
+    for index in xrange(1, 4):
+        cmd = ["iptables", '-D', 'PREROUTING', '-t', 'nat', '-i', interface,
+               "-p", "tcp", "--dport", str(network.sshPortFromHostIndex(index)), "-j", "DNAT",
+               "--to-destination", "%s:22" % network.ipAddressFromHostIndex(index)]
         runCmd(cmd, canFail=True)
-    cmd = ["iptables", "-t", "nat", "-D", "POSTROUTING", "-o", deviceName, "-j", 'MASQUERADE']
+    cmd = ["iptables", "-t", "nat", "-D", "POSTROUTING", "-o", interface, "-j",
+           "MASQUERADE"]
     runCmd(cmd, canFail=True)
 
 
-def writeRules(deviceName):
-    for index in xrange(1, LAST_INDEX + 1):
-        cmd = ["iptables", "-A", "PREROUTING", "-t", "nat", "-i", deviceName,
-               "-p", "tcp", "--dport", str(sshPortFromHostIndex(index)), "-j", "DNAT",
-               "--to", "%s:22" % ipAddressFromHostIndex(index)]
+def writeRules(interface):
+    for index in xrange(1, 4):
+        cmd = ["iptables", "-A", "PREROUTING", "-t", "nat", "-i", interface,
+               "-p", "tcp", "--dport", str(network.sshPortFromHostIndex(index)), "-j", "DNAT",
+               "--to", "%s:22" % network.ipAddressFromHostIndex(index)]
+        print " ".join(cmd)
         runCmd(cmd, canFail=False)
-    cmd = ["iptables", "-t", "nat", "-A", "POSTROUTING", "-o", deviceName, "-j", 'MASQUERADE']
+    cmd = ["iptables", "-t", "nat", "-A", "POSTROUTING", "-o", interface, "-j",
+           "MASQUERADE"]
     runCmd(cmd, canFail=True)
 
 
-def configureNat(deviceName):
-    deleteRules(deviceName)
-    writeRules(deviceName)
+def configureNat(interface):
+    deleteRules(interface)
+    writeRules(interface)
 
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
-        print "Usage: python %(filename)s <interface_name>" % dict(filename=__file__)
+        print "Usage: python %(filename)s <test_client_side_interface>" % dict(filename=__file__)
         sys.exit(1)
 
     handler = logging.StreamHandler()
@@ -48,6 +53,8 @@ if __name__ == "__main__":
     logging.getLogger().addHandler(handler)
     logging.getLogger().setLevel(logging.INFO)
 
-    interfaceName = sys.argv[1]
-    print "Configuring NAT locally for %(interfaceName)s" % dict(interfaceName=interfaceName)
-    configureNat(interfaceName)
+    with open(config.CONFIGURATION_FILE) as f:
+        conf = yaml.load(f.read())
+    network.initialize_globals(conf)
+    interface = sys.argv[2]
+    configureNat(interface)
